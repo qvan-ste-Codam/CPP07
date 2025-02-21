c5a8449a7738aa94a54992bf87fbd06b314aa1c7
accounts@vanstenus.com <qvan-ste@f1r4s8.codam.nl>
2025-02-21 14:13:41 +0100

    added ex02

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,28 +2,35 @@
 #include <string>
 
 #include "iter.hpp"
+template <typename T>
+static void printContents(T val) {
+    std::cout << val << ' ';
+}
 
 int main(void) {
     char a[5] = {'a', 'b', 'c', 'd', 'f'};
     std::cout << "Print char array: ";
-    iter::iter(a, 5, [](char c) { std::cout << c << ' '; });
+    iter::iter(a, 5, printContents<char>);
     std::cout << std::endl;
 
     int b[5] = {1, 2, 3, 4, 5};
     std::cout << "Int array before increment: ";
-    iter::iter(b, 5, [](int i) { std::cout << i << ' '; });
+    iter::iter(b, 5, printContents<int>);
     std::cout << std::endl;
     iter::iter(b, 5, [](int &i) { i++; });
     std::cout << "Int array after increment: ";
-    iter::iter(b, 5, [](int i) { std::cout << i << ' '; });
+    iter::iter(b, 5, printContents<int>);
     std::cout << std::endl;
 
     std::string c[2] = {"hello", "world"};
     std::cout << "String array before change: ";
-    iter::iter(c, 2, [](std::string &s) { std::cout << s << ' '; });
+    iter::iter(c, 2, printContents<std::string &>);
     std::cout << std::endl;
     iter::iter(c, 2, [](std::string &s) { s = "foo"; });
     std::cout << "String array after change: ";
-    iter::iter(c, 2, [](std::string &s) { std::cout << s << ' '; });
+    iter::iter(c, 2, printContents<std::string &>);
     std::cout << std::endl;
+
+    // int d = 5;
+    // iter::iter(d, 1, [](int i) { std::cout << i << ' '; });
 }
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,3 +1,54 @@
-int main (void) {
-}
+#include <iostream>
 
+#include "Array.tpp"
+
+int main() {
+    Array<int> intA(5);
+    std::cout << "Default constructor test: " << intA[0] << std::endl;
+
+    Array<int> intB = intA;
+    std::cout << "Copy constructor test: " << intB[0] << std::endl;
+
+    Array<int> IntC;
+    IntC = intA;
+    std::cout << "Assignment operator test: " << IntC[0] << std::endl;
+
+    intA[0] = 25;
+    std::cout << "Element assignment test (intA[0] = 25): " << intA[0]
+              << std::endl;
+    std::cout << "Check if intB[0] is unchanged: " << intB[0] << std::endl;
+
+    intB[0] = 121738;
+    std::cout << "Element assignment test (intB[0] = 121738): " << intB[0]
+              << std::endl;
+    std::cout << "Check if intA[0] is unchanged: " << intA[0] << std::endl;
+    std::cout << "Check if intC[0] is unchanged: " << IntC[0] << std::endl;
+
+    Array<std::string> stringA(5);
+    std::cout << "Default constructor test: " << stringA[0] << std::endl;
+
+    Array<std::string> stringB = stringA;
+    std::cout << "Copy constructor test: " << stringB[0] << std::endl;
+
+    Array<std::string> stringC;
+    stringC = stringA;
+    std::cout << "Assignment operator test: " << stringC[0] << std::endl;
+
+    stringA[0] = "hello";
+    std::cout << "Element assignment test (StringA[0] = hello): " << stringA[0]
+              << std::endl;
+    std::cout << "Check if stringB[0] is unchanged: " << stringB[0]
+              << std::endl;
+
+    stringB[0] = "world";
+    std::cout << "Element assignment test (stringB[0] = world): " << stringB[0]
+              << std::endl;
+    std::cout << "Check if a[0] is unchanged: " << stringA[0] << std::endl;
+    std::cout << "Check if c[0] is unchanged: " << stringC[0] << std::endl;
+    try {
+        std::cout << "Check out of bounds error " << stringA[10] << std::endl;
+    } catch (std::exception &ex) {
+        std::cerr << "\n" << ex.what() << std::endl;
+    };
+    return 0;
+}
